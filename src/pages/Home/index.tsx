3c1d85ac3d1fc1a911d396cc4b41ec3c40ea73b4
Vinícius Bastos <viniciusbsilva57@gmail.com>
2023-12-08 21:43:02 -0300

    Changed useEffect for useLayoutEffect

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { TrendingItem, getTrending } from "../../services/getTrending";
 import Banner from "../../components/Banner";
 import Carousel from "../../components/Carousel";
@@ -8,7 +8,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [trending, setTrending] = useState<TrendingItem[]>([]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const fetchData = async () => {
       const response = await getTrending({ page: 1, time: "day" });
 
